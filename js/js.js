a0e42817106c09d8cbba1307214b8179da003ff8
julianooselame <julianooselame@hotmail.com>
2022-02-01 14:56:57 -0300

    Limited Player moviment and created Enemy movement loop

--- js/js.js
+++ js/js.js
@@ -16,6 +16,8 @@ function start() {
 		S: 83,
 		D: 68
 	};
+	var velocidade = 5;
+	var posicaoY = parseInt(Math.random() * 334);
 
 	jogo.pressionou = [];
 
@@ -36,6 +38,7 @@ function start() {
 	function loop() {
 		movefundo();
 		movejogador();
+		moveinimigo1();
 	} // Fim da função loop()
 
 	//Função que movimenta o fundo do jogo
@@ -49,15 +52,34 @@ function start() {
 		if (jogo.pressionou[TECLA.W]) {
 			var topo = parseInt($('#jogador').css('top'));
 			$('#jogador').css('top', topo - 10);
+
+			if (topo <= 0) {
+				$('#jogador').css('top', topo + 10);
+			}
 		}
 
 		if (jogo.pressionou[TECLA.S]) {
 			var topo = parseInt($('#jogador').css('top'));
 			$('#jogador').css('top', topo + 10);
+			if (topo >= 434) {
+				$('#jogador').css('top', topo - 10);
+			}
 		}
 
 		if (jogo.pressionou[TECLA.D]) {
 			//Chama função Disparo
 		}
 	} // fim da função movejogador()
+
+	function moveinimigo1() {
+		posicaoX = parseInt($('#inimigo1').css('left'));
+		$('#inimigo1').css('left', posicaoX - velocidade);
+		$('#inimigo1').css('top', posicaoY);
+
+		if (posicaoX <= 0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$('#inimigo1').css('left', 694);
+			$('#inimigo1').css('top', posicaoY);
+		}
+	} //Fim da função moveinimigo1()
 } // Fim da função start
